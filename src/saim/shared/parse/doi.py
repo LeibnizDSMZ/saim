ad77407558eb74465755e8828e41026c2ab5b183
artdotlis <artdotlis@pm.me>
2025-05-15 08:50:49 +0000

    feat: add simple doi verification

--- src/saim/shared/parse/doi.py
+++ src/saim/shared/parse/doi.py
@@ -1,7 +1,15 @@
+import re
 from saim.shared.data_con.designation import STRAIN_SI_ID_DOI
+
+
+_REG_DOI = re.compile(r"^10\.\d{4,9}\/[-._;()/:A-Z0-9]+$")
 
 
 def check_si_id_doi(doi: str, /) -> str:
     if STRAIN_SI_ID_DOI.match(doi) is not None:
         return doi
     raise ValueError(f"DOI {doi} has an invalid StrainInfo format")
+
+
+def is_correct_doi(doi: str, /) -> bool:
+    return _REG_DOI.match(doi) is not None
