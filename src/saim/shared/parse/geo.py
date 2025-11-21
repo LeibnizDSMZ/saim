7ad1d0f9a9ef4c348b27a27701c3e2dac6ffb4f1
artdotlis <artdotlis@pm.me>
2025-11-21 09:58:13 +0000

    refactor(location): clean and validate place names

--- src/saim/shared/parse/geo.py
+++ src/saim/shared/parse/geo.py
@@ -1,8 +1,18 @@
-from typing import Any, Callable
+from re import Pattern
+import re
+from typing import Any, Callable, Final
 
 from cafi.container.country import CountryCodes
 
 from saim.shared.verify.types import ch_str_float
+from saim.shared.parse.string import clean_string, clean_text_rm_enclosing, trim_edges
+
+_PATTERN_BRACKETS: Final[tuple[Pattern[str], ...]] = (
+    re.compile(r"^\([^(]*"),
+    re.compile(r"^\[[^[]*"),
+    re.compile(r"[^(]*\)$"),
+    re.compile(r"[^[]*\]$"),
+)
 
 
 def parse_lat_long(lat_long: Any, ch_spec_float: Callable[[str], str]) -> str:
@@ -27,3 +37,14 @@ def check_country_code(code: Any, /) -> str:
     if len(code) != 2:
         raise ValueError(f"malformed country code {code}")
     return CountryCodes().is_code(code.upper())
+
+
+def clean_place_name(place: str) -> str:
+    place_edgeless = trim_edges(place)
+    if place == "":
+        return ""
+    places_no_para = clean_text_rm_enclosing(place_edgeless)
+    cl_place = clean_string(places_no_para, *_PATTERN_BRACKETS)
+    if len(cl_place) < 2:
+        return ""
+    return cl_place[0].upper() + cl_place[1:]
