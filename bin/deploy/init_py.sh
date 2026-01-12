4c34007a6c27bf4e4afbd7d26240baa041f324bd
Artur Lissin <artdotlis@pm.me>
2026-01-12 15:49:36 +0000

    fix: correct playwright installation

--- bin/deploy/init_py.sh
+++ bin/deploy/init_py.sh
@@ -6,7 +6,7 @@ build="$dist/build.tar.gz"
 
 echo "install required python"
 dnf -y install gcc openssl-devel bzip2-devel libffi-devel \
-    zlib-devel wget make xz
+    zlib-devel wget make xz sudo git
 dnf -y remove python3-pip
 wget "https://www.python.org/ftp/python/$pyv/Python-$pyv.tar.xz"
 tar -xf "Python-$pyv.tar.xz"
@@ -17,5 +17,12 @@ python -V
 cd ..
 echo "finished python"
 python -m pip install "$build"
+echo "install playwright"
+dnf -y install nspr nss dbus-libs at-spi2-atk atk libX11 libXcomposite libXdamage \
+    libXext libXfixes libXrandr mesa-libgbm libxcb \
+    alsa-lib cups-libs xkeyboard-config cairo pango \
+    libxkbcommon
+playwright install chromium
 rm -rf "$dist"
+dnf -y remove git
 echo "project installed"
